ac2e06a7972d1f85c15f4f37764595a7274e8205
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-04-19 17:33:23 +0300

    firebase first commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBUU6IhX7Hux4S4s3h3Ce4s2OKQIbiL1E",
+    authDomain: "super-categories.firebaseapp.com",
+    projectId: "super-categories",
+    storageBucket: "super-categories.appspot.com",
+    messagingSenderId: "759108332706",
+    appId: "1:759108332706:web:c5802d442711dacf8cfbfb",
+    measurementId: "G-R4GKR4BJWN"
+  };
