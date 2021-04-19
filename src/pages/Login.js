489350fdfe8d45b67cd2584cb83753d44d11e357
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-04-19 18:32:43 +0300

    implementing user login with google

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,36 @@
 
 import React,{useState} from 'react'
 import {Form, Button} from 'react-bootstrap'
-import { auth } from '../firebase/firebase'
+import { auth, googleProvider } from '../firebase/firebase'
+import {useDispatch} from 'react-redux'
+import {setUser} from '../redux/actions/UserActions'
+
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const dispatch = useDispatch();
 
 
-    const handleSubmit = (e) => {
+    const handleLogin = (e) => {
         e.preventDefault();
         auth.signInWithEmailAndPassword(email,password);
+    } 
+     const handelLoginWithGoogle = (e) => {
+        e.preventDefault();
+        auth.signInWithPopup(googleProvider).then(async result => {
+            const {user} = result
+                dispatch(setUser({
+                    email: user.email,
+                    displayName: user.displayName,
+                    id: user.uid
+                }))
+        })
     }
     return (
         <div>
            
-            <Form onSubmit={handleSubmit}>
+            <Form>
         <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address / {email}</Form.Label>
             <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}/>
@@ -28,9 +43,8 @@ const Login = () => {
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </Form.Group>
-        <Button variant="primary">
-            Login
-        </Button>
+        <Button variant="primary" onClick={handleLogin}>Login</Button>
+        <Button variant="danger" onClick={handelLoginWithGoogle}>Login With google</Button>
         </Form>
         </div>
     )
