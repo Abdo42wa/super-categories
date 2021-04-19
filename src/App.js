272cedfc4d0d7743436c4d65f42ee1a400d9bbb2
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-04-19 18:03:14 +0300

    importing firebase and  creating a create profile function and also creating a google auth provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Login from './pages/Login';
+import {Container} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
+    <Container>
       <h1>first coomit</h1>
+      <Login />
+
+      </Container>
     </div>
   );
 }
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyCBUU6IhX7Hux4S4s3h3Ce4s2OKQIbiL1E",
     authDomain: "super-categories.firebaseapp.com",
@@ -7,3 +9,39 @@ const firebaseConfig = {
     appId: "1:759108332706:web:c5802d442711dacf8cfbfb",
     measurementId: "G-R4GKR4BJWN"
   };
+
+  firebase.initializeApp(firebaseConfig);
+
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const  userRef = db.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get();
+
+   if (!snapShot.exists) {
+     const {displayName, email} = userAuth;
+     const createdAt = new Date();
+
+     try {
+       await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+       })
+     } catch (error) {
+        console.log(error);
+     }
+
+   }
+   return userRef;
+  }
+
+  const db = firebaseApp.firestore();
+
+  export const auth = firebase.auth();
+
+  export const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+  export default db;
