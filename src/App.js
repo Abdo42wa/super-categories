479b89b34c5d06ccc1bb3557431a087473c1a1cd
PC <lsongulija@gmail.com>
2021-04-19 18:41:50 +0200

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Login from './pages/Login';
 import {Container} from 'react-bootstrap'
+import HomeScreen from './pages/HomeScreen';
 
 function App() {
   return (
     <div className="App">
     <Container>
-      <h1>first coomit</h1>
-      <Login />
+      <HomeScreen/>
+      {/* <Login /> */}
 
       </Container>
     </div>
